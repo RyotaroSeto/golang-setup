f564e4e3ddf9bba821fd479beff3dca83675f60a
RyotaroSeto <hahahahawaiwai321@gmail.com>
2024-09-07 14:47:05 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # golang-setup
+
+## Example
+```
+% goup init
+```
---
+++ cmd/init.go
@@ -0,0 +1,67 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/spf13/cobra"
+)
+
+var initCmd = &cobra.Command{
+	Use:   "init [module name]",
+	Short: "Initialize a new Go module",
+	Long: `Initialize a new Go module with the specified module name.
+This will run 'go mod init', 'go mod tidy', and create a main.go file.`,
+	Args: cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		moduleName := args[0]
+		fmt.Printf("Initializing Go module: %s\n", moduleName)
+
+		if err := runCommand("go", "mod", "init", moduleName); err != nil {
+			return fmt.Errorf("failed to run go mod init: %w", err)
+		}
+
+		if err := runCommand("go", "mod", "tidy"); err != nil {
+			return fmt.Errorf("failed to run go mod tidy: %w", err)
+		}
+
+		if err := createMainGo(); err != nil {
+			return fmt.Errorf("failed to create main.go: %w", err)
+		}
+
+		fmt.Println("Go module initialization complete.")
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+}
+
+func runCommand(name string, args ...string) error {
+	cmd := exec.Command(name, args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+func createMainGo() error {
+	mainFileContent := `package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+`
+	err := os.WriteFile("main.go", []byte(mainFileContent), 0644)
+	if err != nil {
+		return err
+	}
+	fmt.Println("Created main.go")
+	return nil
+}
